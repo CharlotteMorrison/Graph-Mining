ca6293053e04b8f1dcb76fe6d11b042efe8a4974
Ahmsayat <nagar@aucegypt.edu>
2018-03-21 18:48:25 -0400

    Start of DB Project 2

---
+++ DB_Project_2/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Moussa
+ *
+ */
+class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("DB Project 2");
+	}
+}
